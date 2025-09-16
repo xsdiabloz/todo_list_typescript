604f981c5ff6e609834aba5d6a48c612f8e3a2d9
xsdiablozb <cactusjackk48@gmail.com>
2025-09-16 11:49:33 +0300

    add mui

--- src/components/task/Task.tsx
+++ src/components/task/Task.tsx
@@ -1,11 +1,20 @@
 import React, { type FC } from "react";
 import classes from "../task/task.module.css";
+import Checkbox from "@mui/material/Checkbox";
 
 const Task: FC = () => {
   return (
     <div className={classes.task}>
-      <h2>Task 1</h2>
-      <p>Task 1 desc</p>
+      <div className={classes["task-check"]}>
+        <Checkbox />
+      </div>
+
+      <div className={classes["task-info"]}>
+        <h2 className={classes["task-info_title"]}>Task 1</h2>
+        <p className={classes["task-info_desc"]}>Task 1 desc</p>
+      </div>
+
+      <div className={classes["task-actions"]}></div>
     </div>
   );
 };
