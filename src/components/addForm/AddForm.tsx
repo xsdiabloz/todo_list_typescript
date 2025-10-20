cd2fd3537fbe4fa9901a8c2bda864b5587016f9d
xsdiablozb <cactusjackk48@gmail.com>
2025-10-20 16:27:27 +0300

    add a feature

--- src/components/addForm/AddForm.tsx
+++ src/components/addForm/AddForm.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, type FormEvent } from "react";
 import classes from "../addForm/addForm.module.css";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
@@ -7,22 +7,29 @@ const AddForm = () => {
   const [title, setTitle] = useState<string>("");
   const [desc, setDesc] = useState<string>("");
 
+  const addNewTask = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("task added");
+    setTitle("");
+    setDesc("");
+  };
+
   return (
     <>
-      <form className={classes["add-task_form"]}>
+      <form className={classes["add-task_form"]} onSubmit={addNewTask}>
         <h2>New Task</h2>
         <div className={classes["form-title"]}>
           <TextField
             value={title}
             onChange={(e) => setTitle(e.target.value)}
-            placeholder="title..."
+            label="title"
           />
         </div>
         <div className={classes["form-desc"]}>
           <TextField
             value={desc}
             onChange={(e) => setDesc(e.target.value)}
-            placeholder="desc..."
+            label="desc"
           />
         </div>
         <Button type="submit" variant="outlined">
